21c2b5595a25bdf81345fa43903aa843d882188a
Brooke <xbian@applause.com>
2019-05-15 16:01:37 -0400

    Update batch_test.sh

--- batch_test.sh
+++ batch_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Example: run myexample.tsk with argument --gtest_filter=Test.TestABCSuccess 1000 times
+# ./batch_test.sh 1000 myexample.tsk --gtest_filter=Test.TestABCSuccess >/dev/null
+
+
 times=$1
 # get the command and it's argument as a whole
 command=${@:2}
