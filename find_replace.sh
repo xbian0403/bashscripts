efabfa4b38b0ca9dfbc3cf8d19a93b876256b230
Brooke <xbian@applause.com>
2019-07-29 15:17:26 -0400

    Update find_replace.sh

--- find_replace.sh
+++ find_replace.sh
@@ -4,3 +4,10 @@ find . -regex '.*write.*\.py' | xargs sed -i '' 's/attr_read/attr_write/g'
 #rename files of read to write
 files=$(find . -regex '.*read.*')
 for f in $files; do mv "$f" "$(echo "$f" | sed 's/read/write/g')"; done
+
+#replace files content e.g change namespace
+for f in $files; 
+do 
+sed 's/demdatagramtypes/dirgraphtypes/g' -i $f
+sed 's/ddt/dgt/g' -i $f 
+done
