f6a8fee9d18c9ed6336b0394730f793c5cba5099
Brooke <brookebian0403@gmail.com>
2020-01-09 14:20:12 -0800

    Update find_replace.sh

--- find_replace.sh
+++ find_replace.sh
@@ -5,6 +5,11 @@ find . -regex '.*write.*\.py' | xargs sed -i '' 's/attr_read/attr_write/g'
 files=$(find . -regex '.*read.*')
 for f in $files; do mv "$f" "$(echo "$f" | sed 's/read/write/g')"; done
 
+# previous one may not work if it treat files as string instead of an array
+find . -regex '.*read.*' | while read line; do
+mv "$f" "$(echo "$f" | sed 's/read/write/g')";
+done
+
 #replace files content e.g change namespace
 for f in $files; 
 do 
